712d3c00c49bec5f02e36951bd10117a0bdbf568
Serena Peruzzo <serena.peruzzo@gmail.com>
2022-10-02 17:12:03 -0400

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   content: ["src/*.html"],
   theme: {
-    extend: {},
-    colors: {
-      darkseagreen: "#8CBA80",
-      mediumorchid: "#7547DB"
+    extend: {
+      colors: {
+        darkseagreen: "#6CD881",
+        mediumorchid: "#7A31FF"
+      }
     }
   },
   plugins: [],
