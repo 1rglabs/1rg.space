a641ed0afcdbe38a10755d42238eb11918260301
Serena Peruzzo <serena.peruzzo@gmail.com>
2022-10-02 16:08:51 -0400

    change colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["src/*.html"],
   theme: {
     extend: {},
+    colors: {
+      darkseagreen: "#8CBA80",
+      mediumorchid: "#7547DB"
+    }
   },
   plugins: [],
 }
