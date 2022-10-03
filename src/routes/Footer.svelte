bd5bdeb27fcd352635219d1a850ddffbc43f4688
Max Veytsman <maxim@ontoillogical.com>
2022-10-03 12:41:22 -0400

    Rebuild existing site in svelte

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,18 @@
+<!-- TODO padding after font? -->
+<footer class="mb-2 text-black min-h-40 bg-darkseagreen">
+	<div class="flex">
+		<div class="w-1/2"><span class="font-black uppercase text-9xl">join the list</span></div>
+		<div class="flex flex-col justify-end w-1/2">
+			<div class="w-1/2 text-xl font-bold uppercase">
+				subscribe for exclusive updates on our events
+			</div>
+			<div class="mb-2">
+				<input
+					type="email"
+					placeholder="Email"
+					class="px-3 py-1 text-base text-black placeholder-gray-600 border-b-[0.5px] border-gray-600 border-b-gray-700 focus:border-[0.5px] w-72  bg-darkseagreen focus:outline-none "
+				/>
+			</div>
+		</div>
+	</div>
+</footer>
