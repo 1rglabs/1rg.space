fb583d90a7a0beb91c32a12520a7e4d82c375744
Max Veytsman <maxim@ontoillogical.com>
2022-10-03 12:24:09 -0400

    Sveltekit Init

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {
+			colors: {
+				darkseagreen: "#6CD881",
+				mediumorchid: "#7A31FF"
+			}
+		}
+	},
+
+	plugins: []
+};
+
+module.exports = config;
