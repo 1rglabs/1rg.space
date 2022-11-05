0805fdb9364575b3eae0eb89bd2a916c0dcb5da3
Serena Peruzzo <serena.peruzzo@gmail.com>
2022-11-05 15:38:17 -0400

    contact page layout

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import people from '$lib/images/people.jpg'
+</script>
+
+<svelte:head>
+    <title>Contact Us</title>
+</svelte:head>
+
+<section class="flex">
+    <div class="bg-mediumorchid">
+        <h1 class="text-9xl  whitespace-pre-wrap uppercase text-white font-black">Contact Contact Contact</h1>
+    </div>
+</section>
+<section class="flex">
+    <div class="w-1/2">
+        <img 
+          class="object-cover object-right h-full w-full" 
+          src={people}
+          alt="contact us"/>
+    </div>
+    <div class="bg-stone-50">
+        <p>We love to chat.</p>
+        <p>Interested in collaborating with 1RG for an event? Share your ideas with us.</p>
+        <p>Looking for a space to host your group or meeting?  Ask us about our boardroom rentals.</p>
+        <p>Want to become a member? We’d love to see you.</p>
+    </div>
+</section>
