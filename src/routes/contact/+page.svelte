80b8fbfc0bd9ed8d0a7a4ea984fd6c70902cf31a
Serena Peruzzo <serena.peruzzo@gmail.com>
2022-11-06 16:18:39 -0500

    format right-hand side

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,27 +1,36 @@
 <script>
-    import people from '$lib/images/people.jpg'
+	import people from '$lib/images/people.jpg';
 </script>
 
 <svelte:head>
-    <title>Contact Us</title>
+	<title>Contact Us</title>
 </svelte:head>
 
 <section class="flex">
-    <div class="bg-mediumorchid">
-        <h1 class="text-9xl  whitespace-pre-wrap uppercase text-white font-black">Contact Contact Contact</h1>
-    </div>
+	<div class="bg-mediumorchid">
+		<h1 class="text-9xl  whitespace-pre-wrap uppercase text-white font-black">
+			TODO Contact Contact Contact
+		</h1>
+	</div>
 </section>
 <section class="flex">
-    <div class="w-1/2">
-        <img 
-          class="object-cover object-right h-full w-full" 
-          src={people}
-          alt="contact us"/>
-    </div>
-    <div class="bg-stone-50">
-        <p>We love to chat.</p>
-        <p>Interested in collaborating with 1RG for an event? Share your ideas with us.</p>
-        <p>Looking for a space to host your group or meeting?  Ask us about our boardroom rentals.</p>
-        <p>Want to become a member? We’d love to see you.</p>
-    </div>
+	<div class="w-1/2">
+		<img class="object-cover object-right h-full w-full" src={people} alt="contact us" />
+	</div>
+	<div class="w-1/2 bg-stone-50 flex flex-col">
+		<div class="text-2xl items-center justify-center p-14 font-normal">
+			<p class="mb-4"><span>We love to chat.</span></p>
+			<p class="mb-4">
+				<span>Interested in collaborating with 1RG for an event? Share your ideas with us.</span>
+			</p>
+			<p class="mb-4">
+				<span>Looking for a space to host your group or meeting? Ask us about our boardroom rentals.</span>
+			</p>
+			<p class="mb-4"><span>Want to become a member? We’d love to see you.</span></p>
+		</div>
+		<div class="items-center justify-center pl-14">
+			<h3 class="text-3xl text-mediumorchid uppercase font-bold ">Email:</h3>
+			<p class="mb-4 text-2xl"><span>team@1rg.space</span></p>
+		</div>
+	</div>
 </section>
