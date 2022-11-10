2d2ee8067fd74c036b896e1c2acbab1862bef382
Max Veytsman <maxim@ontoillogical.com>
2022-11-09 19:44:43 -0500

    Initial layout for about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,26 +1,50 @@
+<script>
+	import couch_conversation from '$lib/images/couch_conversation.jpg';
+
+	import Heading from '$lib/components/Heading.svelte';
+</script>
+
 <svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
+	<title>About Us</title>
 </svelte:head>
 
-<div class="text-column">
-	<h1>About this app</h1>
+<Heading>About Us About Us About Us</Heading>
 
+<section class="p-20 text-4xl font-light leading-normal text-center text-gray-300 bg-black">
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
+		1RG invites individuals, teams and organizations who value community care, creativity and
+		innovation to work and play in a space that promotes collaboration and curiosity.
 	</p>
+</section>
+<section class="flex flex-col">
+	<div
+		class="p-20 text-lg font-bold tracking-[.3em] text-center text-white uppercase bg-mediumorchid"
+	>
+		Making Space for Everyone
+	</div>
+	<div class="p-16 text-black bg-white">
+		<img class="mx-auto grayscale" src={couch_conversation} alt="Conversation on the couch" />
+		<p class="mt-12 text-xl text-center">
+			Our space includes members from non-profits, community organizers, freelance creatives and
+			remote tech teams. Everyone is welcome at 1RG and we believe that the diversity of our members
+			is what generates lasting change.
+		</p>
+	</div>
+</section>
+<section class="flex flex-col p-16 text-center text-white bg-black">
+	<div class="">
+		<p>
+			“1RG Provides a space where I can connect with like minded individuals who have a strong
+			community connection”
+		</p>
+		<p class="mt-8">— Muzen —</p>
+	</div>
 
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
-</div>
+	<div class="mt-28">
+		<p>
+			“I've had the priveledge to attend one of the open mic nights at 1RG and it was an amazing
+			experience!”
+		</p>
+		<p class="mt-8">— Lauren M —</p>
+	</div>
+</section>
