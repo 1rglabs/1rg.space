37f983f3b644c20c2e1e9204e21e1e9da5446358
Max Veytsman <maxim@ontoillogical.com>
2022-11-09 20:02:51 -0500

    Margin

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -8,7 +8,7 @@
 
 <header class="flex h-20 bg-mediumorchid">
 	<a class="text-white hover:text-darkseagreen" href="/">
-		<Logo class="justify-center h-20 ml-24" />
+		<Logo class="justify-center h-20 lg:ml-24" />
 	</a>
 	<div class="flex items-center justify-end w-full">
 		<HamburgerButton on:click={() => (menuOpen = true)} />
