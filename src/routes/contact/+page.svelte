302a37462e995da45b2d13918a5efa9fdc6e6ed1
Max Veytsman <maxim@ontoillogical.com>
2022-11-17 17:16:05 -0500

    Fix portrait

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -9,9 +9,13 @@
 </svelte:head>
 
 <Heading>Contact Contact Contact</Heading>
-<section class="flex flex-col md:flex-row">
-	<div class="md:w-1/2 h-48 md:h-[56rem] lg:h-[48rem]">
-		<img class="object-cover object-[50%_50%] w-full h-full" src={serena_serving} alt="contact us" />
+<section class="flex flex-col md:items-stretch md:flex-row">
+	<div class="h-48 md:h-[unset] md:w-1/2">
+		<img
+			class="w-full h-full object-cover object-[50%_30%]"
+			src={serena_serving}
+			alt="contact us"
+		/>
 	</div>
 	<div class="flex flex-col md:w-1/2 bg-stone-50">
 		<div class="p-16 m-auto text-2xl md:max-w-2xl">
@@ -24,7 +28,9 @@
 			</p>
 			<p class="mb-4">Want to become a member? We’d love to see you.</p>
 			<h3 class="mt-10 text-3xl font-bold uppercase text-mediumorchid">Email:</h3>
-			<a href="mailto:team@1rg.space" class="mb-4 text-2xl hover:text-mediumorchid">team@1rg.space</a>
+			<a href="mailto:team@1rg.space" class="mb-4 text-2xl hover:text-mediumorchid"
+				>team@1rg.space</a
+			>
 		</div>
 	</div>
 </section>
