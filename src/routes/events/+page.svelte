983904e69e7c275301fbbf90b39dc0367b1b9007
Max Veytsman <maxim@ontoillogical.com>
2022-11-18 11:30:47 -0500

    Added a dummy event

---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Heading from '$lib/components/Heading.svelte';
+</script>
+
+<svelte:head>
+	<title>Events</title>
+</svelte:head>
+
+<Heading>Events Events Events</Heading>
+<section class="flex flex-col px-20 py-16 bg-white">
+	<div class="py-16 border-t-2 border-b-2 border-mediumorchid">
+		<div class="md:relative md:text-center">
+			<div class="text-xl font-semibold uppercase md:absolute md:top-0 md:left-0 text-mediumorchid">Dec 16, 2022</div>
+			<div class="py-5 text-lg font-bold uppercase md:py-0 lg:text-2xl">Holiday/Closing Party</div>
+		</div>
+		<p class="py-6">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec
+			ullamcorper mattis, pulvinar dapibus leo.
+		</p>
+	</div>
+</section>
