b4f233254c21e9f629a2e8b3a0f6b5a6f92581ae
Max Veytsman <maxim@ontoillogical.com>
2022-11-18 13:21:16 -0500

    Fix heading overflow

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -1,7 +1,7 @@
 <section class="flex bg-mediumorchid">
-	<div class="flex justify-center mt-5 mb-8 overflow-hidden -mx-36 sm:mx-0">
+	<div class="flex justify-center mt-5 mb-8 overflow-hidden ">
 		<h1
-			class="font-black text-center text-white uppercase md:whitespace-normal text-7xl md:text-8xl xl:text-9xl"
+			class="font-black text-center text-white uppercase -mx-36 sm:mx-0 md:whitespace-normal text-7xl md:text-8xl xl:text-9xl"
 		>
 			<slot />
 		</h1>
