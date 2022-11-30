1579683d68e718f80a12a87bbbe46ea47b1fb807
Max Veytsman <maxim@ontoillogical.com>
2022-11-30 17:26:57 -0500

    Holiday event

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -10,12 +10,17 @@
 <section class="flex flex-col px-20 py-16 bg-white">
 	<div class="py-16 border-t-2 border-b-2 border-mediumorchid">
 		<div class="md:relative md:text-center">
-			<div class="text-xl font-semibold uppercase md:absolute md:top-0 md:left-0 text-mediumorchid">Dec 16, 2022</div>
+			<div class="text-xl font-semibold uppercase md:absolute md:top-0 md:left-0 text-mediumorchid">
+				Dec 16, 2022
+			</div>
 			<div class="py-5 text-lg font-bold uppercase md:py-0 lg:text-2xl">Holiday/Closing Party</div>
 		</div>
-		<p class="py-6">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec
-			ullamcorper mattis, pulvinar dapibus leo.
-		</p>
+		<div class="py-6">
+			<p class="">A Toast Beofore We Say Goodbye.</p>
+			<p class="mt-3">
+				You’re invited to toast the holidays and say goodbye to Roof Garden - in case you missed it,
+				we’re moving in 2023.
+			</p>
+		</div>
 	</div>
 </section>
