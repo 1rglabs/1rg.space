1a5948c2dfb7318b4621fdd5f3efb05e983c23fb
teesloane <tyler.sloane@testdouble.com>
2023-05-09 14:35:56 -0400

    Add: better hero for labs/

--- src/routes/(labs)/labs/+page.svelte
+++ src/routes/(labs)/labs/+page.svelte
@@ -1,59 +1,89 @@
 <script>
   import portrait from '$lib/images/portraits/047A5808.jpg';
-  import Heading from '$lib/components/Heading.svelte';
   import SectionHeading from '$lib/components/SectionHeading.svelte';
+	import Logo from '$lib/components/Logo.svelte';
+	import { fade } from 'svelte/transition';
+  import { onMount } from 'svelte'
+
+  let runFade = false;
+  let showArrow = false;
+  setTimeout(() => {runFade = true}, 2000)
+  setTimeout(() => {runFade = false}, 8000)
+  onMount(() => {
+    showArrow = document.getElementById("hero").offsetHeight > 700
+  })
+
 </script>
 
 <svelte:head>
   <title>1RG Labs</title>
 </svelte:head>
 
-<Heading>Labs Labs Labs</Heading>
+<div id="hero" class="flex flex-col bg-black ">
+  <div class="w-full flex flex-col md:flex-row justify-center items-center bg-black h-96 md:h-screen relative">
+    <div class="mr-4">
+      <div class="bg-mediumorchid w-64">
+        <Logo class="bg-darkseagreen"/>
+      </div>
+    </div>
 
-<section class="flex justify-center p-10 bg-black md:p-20">
-  <p class="max-w-6xl text-lg xl:text-4xl font-light leading-normal text-center text-gray-300 ">
+    <div class="text-white mt-4 md:mt-0 text-6xl xl:text-9xl font-bold">Labs.</div>
+  </div>
+
+  {#if runFade && showArrow}
+    <div transition:fade="{{duration: 800 }}" class="w-12 absolute left-0 right-0 m-auto bottom-32 animate-pulse">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" class="w-12 h-12">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
+    </svg>
+  </div>
+  {/if}
+
+</div>
+
+<section class="flex justify-center bg-black pb-24">
+  <p class="max-w-6xl text-md xl:text-2xl font-light leading-normal text-center text-gray-300 px-12">
     1RG Labs exists to provide NGOs, non-profits, and socially impactful organizations with the kind of software & data expertise that's normally reserved for Big Tech. Primarily, we operate as a fractional tech team in order to deliver these services at a level that’s financially sustainable to the organizations we serve.
   </p>
 </section>
 <!-- Our Framework -->
-  <SectionHeading sectionTitle="Our Framework" >
-    <p class="mb-12 xl:mt:16">
-      1RG Labs operates differently than a typical consultancy or design studio. Our core belief is that technology is an amplifier that allows organizations to maximize their impact, but deciding how to use technology requires trusted expertise and data-driven decision making.
-    </p>
-    <p class="mb-12">
-      Our experience has been that there is a disconnect between the intuitions of consumers and producers of technology - on the one hand, things that appear to be difficult and expensive to a client are known to be easy and cheap to a technologist. On the other hand, technologists tend to overemphasize technical solutions to social problems.
-    </p>
-    <p class="mb-12">
-      We seek to bridge this gap by embedding with organizations that don’t have internal expertise to be their fractional tech team. Our focus is on long-term relationships, providing data insights and expertise to support decision making, and using our close and ongoing relationship with clients to find and implement low-effort/high-impact solutions.
-    </p>
-  </SectionHeading>
+<SectionHeading sectionTitle="Our Framework" >
+  <p class="mb-12 xl:mt:16">
+    1RG Labs operates differently than a typical consultancy or design studio. Our core belief is that technology is an amplifier that allows organizations to maximize their impact, but deciding how to use technology requires trusted expertise and data-driven decision making.
+  </p>
+  <p class="mb-12">
+    Our experience has been that there is a disconnect between the intuitions of consumers and producers of technology - on the one hand, things that appear to be difficult and expensive to a client are known to be easy and cheap to a technologist. On the other hand, technologists tend to overemphasize technical solutions to social problems.
+  </p>
+  <p class="mb-12">
+    We seek to bridge this gap by embedding with organizations that don’t have internal expertise to be their fractional tech team. Our focus is on long-term relationships, providing data insights and expertise to support decision making, and using our close and ongoing relationship with clients to find and implement low-effort/high-impact solutions.
+  </p>
+</SectionHeading>
 
 
 <!-- Three Pillars -->
-  <SectionHeading sectionTitle="Three Pillars" isReversed={true}>
-    <p>The 1RG Labs framework is centered around three pillars:</p>
-    <p class="mb-12 mt-8">
-      Domain knowledge: our team is embedded in the organization, with regular check-ins serving as a primary touch point to gain deep understanding of the mission, priorities, and pain points of the organization as it operates day to day.
-    </p>
+<SectionHeading sectionTitle="Three Pillars" isReversed={true}>
+  <p>The 1RG Labs framework is centered around three pillars:</p>
+  <p class="mb-12 mt-8">
+    Domain knowledge: our team is embedded in the organization, with regular check-ins serving as a primary touch point to gain deep understanding of the mission, priorities, and pain points of the organization as it operates day to day.
+  </p>
 
-    <p class="mb-12">
-      Technical excellence: our team collectively has two decades of experience with experience both in data science and software development, with a track record for building products and leveraging technology to help organizations achieve their missions.
-    </p>
-    <p class="mb-12">
-      Prioritization under constraints: mastery of technology and tooling, paired with a deep understanding of the organizations we work with, allow our team to identify the areas where these can be most effective, given the priorities of the organization and the resource constraints.
-    </p>
+  <p class="mb-12">
+    Technical excellence: our team collectively has two decades of experience with experience both in data science and software development, with a track record for building products and leveraging technology to help organizations achieve their missions.
+  </p>
+  <p class="mb-12">
+    Prioritization under constraints: mastery of technology and tooling, paired with a deep understanding of the organizations we work with, allow our team to identify the areas where these can be most effective, given the priorities of the organization and the resource constraints.
+  </p>
 
-    <p>
-      The core of this embedding is a biweekly check-in with the client which structures the rest of our work. The goal is to learn what’s going on in the organization, provide advice, and find ways that we can apply our skills to help that the client might not have anticipated.
-    </p>
-  </SectionHeading>
+  <p>
+    The core of this embedding is a biweekly check-in with the client which structures the rest of our work. The goal is to learn what’s going on in the organization, provide advice, and find ways that we can apply our skills to help that the client might not have anticipated.
+  </p>
+</SectionHeading>
 
 <div class="flex w-100 bg-mediumorchid justify-center">
-		<div class="p-12 text-white bg-mediumorchid md:flex md:items-center">
-			<p class="text-lg leading-loose xl:text-8xl md:text-xl md:leading-[4.5rem] xl:leading-none xl:tracking-widest font-semibold tracking-[.3em] text-center uppercase" >
-				The Team
-			</p>
-		</div>
+  <div class="p-12 text-white bg-mediumorchid md:flex md:items-center">
+    <p class="text-lg leading-loose xl:text-8xl md:text-xl md:leading-[4.5rem] xl:leading-none xl:tracking-widest font-semibold tracking-[.3em] text-center uppercase" >
+      The Team
+    </p>
+  </div>
 </div>
 
 <section class="flex flex-col text-white bg-black xl:items-stretch xl:flex-row pb-24">
@@ -71,6 +101,6 @@
     </p>
   </div>
   <div class="flex justify-center p-16 -mt-20 xl:w-1/2 xl:mt-0">
-    <img class="object-cover object-center w-4/5 h-full xl:w-full" src={portrait} alt="Max & Serena"/>
+    <img class="object-cover object-center h-full xl:w-full" src={portrait} alt="Max & Serena"/>
   </div>
 </section>
