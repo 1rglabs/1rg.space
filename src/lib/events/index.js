c60e9e995c4cc21f0d703733f64826213c27f956
teesloane <tyler.sloane@testdouble.com>
2023-06-02 15:36:04 -0400

    Add: An events json file for easier updating of events.

---
+++ src/lib/events/index.js
@@ -0,0 +1,47 @@
+/** example event:
+
+{
+  name: "Event Name",
+  date: "Dec 15, 2022",
+  time: {
+    "start": "17:00",
+    "end": "20:00",
+  },
+  description: "",
+  inviteLink: {
+    linkText: "",
+    linkUrl: "",
+}
+
+*
+*/
+
+export default [
+
+
+  {
+    name: "Winter Solstice: Restorative Yoga + Soundbath",
+    date: "Dec 15, 2022",
+    time: {
+      "start": null,
+      "end": null,
+    },
+    description: "Invite in rest and stress release through a gentle 60-minute restorative practice, while being bathed in healing sound vibrations.",
+    invite: {
+      text: "RSVP",
+      url: "https://www.eventbrite.ca/e/winter-solstice-restorative-yoga-soundbath-tickets-481790677887",
+    }
+  },
+
+  {
+    name: "Holiday/Closing Party",
+    date: "Dec 16, 2022",
+    time: {
+      start: null,
+      end: null,
+    },
+    description: "A Toast Before We Say Goodbye.",
+    invite: null
+  },
+
+]
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,6 +1,7 @@
 <script>
-	import Heading from '$lib/components/Heading.svelte';
-	import Button from '$lib/components/Button.svelte';
+ import Heading from '$lib/components/Heading.svelte';
+ import Button from '$lib/components/Button.svelte';
+ import events from '$lib/events/';
 </script>
 
 <svelte:head>
@@ -8,38 +9,28 @@
 </svelte:head>
 
 <Heading>Events Events Events</Heading>
+
 <section class="flex flex-col px-20 py-16 bg-white">
-	<div class="py-16 border-t-2 border-b-2 border-mediumorchid">
-		<div class="md:relative md:text-center">
-			<div class="text-xl font-semibold uppercase md:absolute md:top-0 md:left-0 text-mediumorchid">
-				Dec 15, 2022
-			</div>
-			<div class="py-5 text-lg font-bold uppercase md:py-0 lg:text-2xl">Winter Solstice: Restorative Yoga + Soundbath</div>
-		</div>
-		<div class="py-6">
-			<p class="mt-3">
-				Invite in rest and stress release through a gentle 60-minute restorative practice, while being bathed in healing sound vibrations.
-			</p>
-		</div>
-		<div>
-			<Button href="https://www.eventbrite.ca/e/winter-solstice-restorative-yoga-soundbath-tickets-481790677887" class="px-6 py-2 m-1 text-m font-black leading-tight xl:leading-normal">
-			RSVP
-			</Button>
-		</div>
-	</div>
-	<div class="py-16 border-t-2 border-b-2 border-mediumorchid">
-		<div class="md:relative md:text-center">
-			<div class="text-xl font-semibold uppercase md:absolute md:top-0 md:left-0 text-mediumorchid">
-				Dec 16, 2022
-			</div>
-			<div class="py-5 text-lg font-bold uppercase md:py-0 lg:text-2xl">Holiday/Closing Party</div>
-		</div>
-		<div class="py-6">
-			<p class="">A Toast Beofore We Say Goodbye.</p>
-			<p class="mt-3">
-				You’re invited to toast the holidays and say goodbye to Roof Garden - in case you missed it,
-				we’re moving in 2023.
-			</p>
-		</div>
-	</div>
+  {#each events as event }
+    <div class="py-16 border-t-2 border-b-2 border-mediumorchid">
+ 	<div class="md:relative md:text-center">
+ 		<div class="text-xl font-semibold uppercase md:absolute md:top-0 md:left-0 text-mediumorchid">
+ 			{event.date}
+ 		</div>
+ 		<div class="py-5 text-lg font-bold uppercase md:py-0 lg:text-2xl">{event.name}</div>
+ 	</div>
+ 	<div class="py-6">
+ 		<p class="mt-3">
+ 			{event.description}
+ 		</p>
+ 	</div>
+    {#if event.invite}
+ 	<div>
+ 		<Button href="{event.invite.url}" class="px-6 py-2 m-1 text-m font-black leading-tight xl:leading-normal">
+ 		{event.invite.text}
+ 		</Button>
+ 	</div>
+    {/if}
+ </div>
+ {/each}
 </section>
