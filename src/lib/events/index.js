675c1964440f4a01600fdc09dcfeaf5098807f27
Max Veytsman <maxim@ontoillogical.com>
2023-06-07 23:39:37 -0400

    Add some upcoming events

--- src/lib/events/index.js
+++ src/lib/events/index.js
@@ -21,13 +21,39 @@
 
 export default [
   {
+    name: "DWeb YYZ June Meetup",
+    date: "June 15, 2023",
+    time: {
+      start: "6pm",
+      end: "8:30pm"
+    },
+    description: "​Mauve will give a short intro on using the p2p web and get folks set up with with Agregore",
+    invite: {
+      text: "RSVP",
+      url: "https://lu.ma/ji5oo2gv",
+    }
+  },
+  {
+    name: "Felt Toronto Meetup",
+    date: "July 3, 2023",
+    time: {
+      start: "6pm",
+      end: "8:30pm"
+    },
+    description: "Join us for a series of lightning talks and a mapping party.",
+    invite: {
+      text: "RSVP",
+      url: "https://lu.ma/felt-toronto",
+    }
+  },
+  {
     name: "DWeb YYZ August Meetup",
     date: "August 30, 2023",
     time: {
       start: "6pm",
       end: "9pm"
     },
-    description: "Todo",
+    description: "​Lightning talks? Snacks? Demos? Hacking! Let us know in the sign up what you'd like to do.",
     invite: {
       text: "RSVP",
       url: "https://lu.ma/ckuvu4l9",
