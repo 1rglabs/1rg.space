80e23727faeacbf93794aa146574269197cfccfd
Serena Peruzzo <serena.peruzzo@gmail.com>
2023-09-17 22:11:36 -0400

    membership card component

--- src/routes/memberships/+page.svelte
+++ src/routes/memberships/+page.svelte
@@ -2,6 +2,26 @@
 	import boardroom from '$lib/images/1rg/047A6732.jpg';
 
 	import Heading from '$lib/components/Heading.svelte';
+	import MembershipTierCard from '$lib/components/MembershipTierCard.svelte';
+
+	let membershipCardCopy = {
+		"Community Membership": {
+			description: "You're part of the 1RG community, but don't need a workspace. You'll get access to Tuesday dinners, and a weekly open house on Thursday where you can come use the space during the day for co-working, or just to drop by!",
+			access: "Access to 1RG on Thursday between 8am and 6pm",
+			price: "100"
+		},
+		"Hot Desk": {
+			description: "1RG is part of your rountine but you're not working here every day. You'll get access during business hours and can use any of the vailable desks. This is for people that want to be engaged in the community and love working from here, but don't need a committed office.",
+			access: "Access to 1RG Monday to Friday between 8am and 6pm",
+			price: "300"
+		},
+		"Dedicated Desk": {
+			description: "1RG is your permanent office. You'll get 24/7 access with keys, a dedicated desk, and the ability to throw events in the space (up to 30 people). This is for people who want to actively engage in helping shape the space and community.",
+			access: "Access to 1RG 24/7",
+			price: "500",
+			cancellationPolicy: "6 months commitment required"
+		}
+	}
 
 </script>
 
@@ -25,7 +45,6 @@
 		<p>Interested in joining? Email us at <a class="hover:text-darkseagreen" href="mailto:team@1rg.space">team@1rg.space</a></p>
 	</div>
 </section> -->
-
 <section class="flex flex-col my-24 p-8">
 	<div class="text-center flex flex-col items-center">
 		<div class="text-2xl">Please,</div>
@@ -33,70 +52,9 @@
 	</div>
 
 	<div class="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-16 p-8">
-
-		<!-- Community Membership -->
-			<div class="flex flex-col p-8 border-2 rounded-md border-slate-300 shadow">
-				<div class="text-center font-bold uppercase text-lg lg:text-xl tracking-wide text-mediumorchid">Community Membership</div>
-				<div class="mt-4 text-center font text-m lg:text-lg">
-					
-					You're part of the 1RG community, but don't need a workspace. You'll get access to Tuesday dinners, and a weekly open house on Thursday where you can come use the space during the day for co-working, or just to drop by!
-				</div>
-				<div class="mt-4 font text-m lg:text-lg">
-					What's included:
-					<ul class="list-disc list-inside ml-4">
-						<li>Tuesday Community Dinners</li>
-						<li>Access to 1RG on Thursday between 8am and 6pm</li>
-						<li>Any future community event</li>
-						<li>Open guest policy</li>
-					</ul>
-				</div>
-				<div class="mt-10 text-center">
-					<p class="font-bold text-lg lg:text-xl">$100/month + HST</p>
-					<p class="text-m lg:text-lg">cancel any time</p>
-				</div>
-			</div>
-
-			<!-- Hot Desk -->
-			<div class="flex flex-col p-8 border-2 rounded-md border-slate-300 shadow">
-				<div class="text-center font-bold uppercase text-lg lg:text-xl tracking-wide text-mediumorchid">Hot Desk</div>
-				<div class="mt-4 text-center font text-m lg:text-lg">
-					1RG is part of your rountine but you're not working here every day. You'll get access during business hours and can use any of the vailable desks. This is for people that want to be engaged in the community and love working from here, but don't need a committed office.
-				</div>
-				<div class="mt-4 font text-m lg:text-lg">
-					What's included:
-					<ul class="list-disc list-inside ml-4">
-						<li>Tuesday Community Dinners</li>
-						<li>Access to 1RG Monday to Friday between 8am and 6pm</li>
-						<li>Any future community event</li>
-						<li>Open guest policy</li>
-					</ul>
-				</div>
-				<div class="mt-10 text-center">
-					<p class="font-bold text-lg lg:text-xl">$300/month + HST</p>
-					<p class="text-m lg:text-lg">cancel any time</p>
-				</div>
-			</div>
-			
-			<!-- Dedicated Desk -->
-			<div class="flex flex-col p-8 border-2 rounded-md border-slate-300 shadow">
-				<div class="text-center font-bold uppercase text-lg lg:text-xl tracking-wide text-mediumorchid">Dedicated Desk</div>
-				<div class="mt-4 text-center font text-m lg:text-lg">
-					1RG is your permanent office. You'll get 24/7 access with keys, a dedicated desk, and the ability to throw events in the space (up to 30 people). This is for people who want to actively engage in helping shape the space and community.
-				</div>
-				<div class="mt-4 font text-m lg:text-lg">
-					What's included:
-					<ul class="list-disc list-inside ml-4">
-						<li>Tuesday Community Dinners</li>
-						<li>Access to 1RG 24/7</li>
-						<li>Any future community event</li>
-						<li>Open guest policy</li>
-					</ul>
-				</div>
-				<div class="mt-10 text-center">
-					<p class="font-bold text-lg lg:text-xl">$500/month + HST</p>
-					<p class="text-m lg:text-lg">6 months commitment required</p>
-				</div>
-			</div>
+		{#each Object.entries(membershipCardCopy) as [tier, vals]}
+			<MembershipTierCard tier={tier} description={vals.description} access={vals.access} price={vals.price}></MembershipTierCard>
+		{/each}
 	</div>
 
 	<div class="text-center flex flex-col items-center mt-20">
