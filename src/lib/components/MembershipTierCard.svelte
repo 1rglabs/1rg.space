ef0bf6a909abfd65cbcf23a1656423f1f950e04f
Serena Peruzzo <sereprz@users.noreply.github.com>
2023-09-19 15:49:13 -0400

    Add membership tiers to the website (#25)
    * Try: memberships cards
    
    * updated copy
    
    * reduce size
    
    * membership card component
    
    * remove comments
    
    ---------
    
    Co-authored-by: teesloane <tyler.sloane@testdouble.com>

---
+++ src/lib/components/MembershipTierCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let tier: string;
+    export let description: string;
+    export let access: string;
+    export let price: string;
+    export let cancellationPolicy: string | undefined = "cancel any time";
+
+</script>
+
+<div class="grid grid-rows-6 p-8 border-2 rounded-md border-slate-300 shadow">
+    <div class="row-span-1 text-center mt-4 font-bold uppercase text-xl lg:text-2xl tracking-wide text-mediumorchid">{tier}</div>
+    <div class="row-span-2 text-center font text-m lg:text-lg">
+        {description}
+    </div>
+    <div class="row-span-2 mt-2 font text-m lg:text-lg">
+        What's included:
+        <ul class="list-disc list-inside ml-4">
+            <li>Tuesday Community Dinners</li>
+            <li>{access}</li>
+            <li>Any future community event</li>
+            <li>Open guest policy</li>
+        </ul>
+    </div>
+    <div class="row-span-1 mt-4 text-center">
+        <p class="font-bold text-lg lg:text-xl">${price}/month + HST</p>
+        <p class="text-m lg:text-lg">{cancellationPolicy}</p>
+    </div>
+</div>
--- src/routes/memberships/+page.svelte
+++ src/routes/memberships/+page.svelte
@@ -2,6 +2,27 @@
 	import boardroom from '$lib/images/1rg/047A6732.jpg';
 
 	import Heading from '$lib/components/Heading.svelte';
+	import MembershipTierCard from '$lib/components/MembershipTierCard.svelte';
+
+	let membershipCardCopy = {
+		"Community Membership": {
+			description: "You're part of the 1RG community, but don't need a workspace. You'll get access to Tuesday dinners, and a weekly open house on Thursday where you can come use the space during the day for co-working, or just to drop by!",
+			access: "Access to 1RG on Thursday between 8am and 6pm",
+			price: "100"
+		},
+		"Hot Desk": {
+			description: "1RG is part of your rountine but you're not working here every day. You'll get access during business hours and can use any of the vailable desks. This is for people that want to be engaged in the community and love working from here, but don't need a committed office.",
+			access: "Access to 1RG Monday to Friday between 8am and 6pm",
+			price: "300"
+		},
+		"Dedicated Desk": {
+			description: "1RG is your permanent office. You'll get 24/7 access with keys, a dedicated desk, and the ability to throw events in the space (up to 30 people). This is for people who want to actively engage in helping shape the space and community.",
+			access: "Access to 1RG 24/7",
+			price: "500",
+			cancellationPolicy: "6 months commitment required"
+		}
+	}
+
 </script>
 
 <svelte:head>
@@ -9,19 +30,21 @@
 </svelte:head>
 
 <Heading>Memberships Memberships Memberships</Heading>
-<section class="flex flex-col md:items-stretch md:flex-row">
-	<div class="h-64 md:h-[unset] md:w-1/2">
-		<img
-			class="object-cover object-center w-full h-full grayscale"
-			src={boardroom}
-			alt="boardroom meeting"
-		/>
+<section class="flex flex-col my-24 p-8">
+	<div class="text-center flex flex-col items-center">
+		<div class="text-2xl">Please,</div>
+		<h1 class="px-6 py-5 md:m-4 text-4xl mt-0 pt-0 font-black leading-tight md:text-6xl xl:text-8xl xl:leading-normal text-center uppercase">Seat yourself </h1>
 	</div>
 
-	<div
-		class="flex flex-col justify-center p-16 space-y-10 text-xl font-semibold leading-10 tracking-widest text-center uppercase lg:leading-loose lg:text-2xl md:w-1/2 bg-stone-50 text-mediumorchid"
-	>
-		<p>Interested in joining? Email us at <a class="hover:text-darkseagreen" href="mailto:team@1rg.space">team@1rg.space</a></p>
+	<div class="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-16 p-8">
+		{#each Object.entries(membershipCardCopy) as [tier, vals]}
+			<MembershipTierCard tier={tier} description={vals.description} access={vals.access} price={vals.price}></MembershipTierCard>
+		{/each}
+	</div>
 
+	<div class="text-center flex flex-col items-center mt-20">
+		<p class="flex flex-col justify-center text-xl font-semibold leading-10 tracking-widest text-center uppercase lg:leading-loose lg:text-2xl md:w-1/2 text-mediumorchid">
+			Interested in joining? Email us at <a class="hover:text-darkseagreen" href="mailto:team@1rg.space">team@1rg.space</a>
+		</p>
 	</div>
 </section>
