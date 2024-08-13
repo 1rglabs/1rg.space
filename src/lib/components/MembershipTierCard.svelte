7d5f29c780bf83ce62f2c69228dee0ede825e34e
Serena Peruzzo <sereprz@users.noreply.github.com>
2024-08-13 14:52:34 -0400

    40 improve membership mobile view (#41)
    * Move CTA to the top and make the button sticky on mobile
    
    * Update membership descriptions

--- src/lib/components/MembershipTierCard.svelte
+++ src/lib/components/MembershipTierCard.svelte
@@ -7,23 +7,28 @@
 
 </script>
 
-<div class="grid grid-rows-6 p-8 border-2 rounded-md border-slate-300 shadow">
-    <div class="row-span-1 text-center mt-4 font-bold uppercase text-xl lg:text-2xl tracking-wide text-mediumorchid">{tier}</div>
-    <div class="row-span-2 text-center font text-m lg:text-lg">
+<div class="grid grid-rows-7 p-2 md:p-8 border-2 rounded-md border-slate-300 shadow">
+    <div class="row-span-1 text-center mt-4 font-bold uppercase text-lg lg:text-xl tracking-wide text-mediumorchid">{tier}</div>
+    <div class="row-span-2 text-center font text-sm lg:text-lg">
         {description}
     </div>
-    <div class="row-span-2 mt-2 font text-m lg:text-lg">
+    <div class="row-span-3 font text-sm lg:text-lg">
         What's included:
         <ul class="list-disc list-inside ml-4">
-            <li>Community events every Tuesday</li>
-            <li>Monthly Community Dinner</li>
             <li>{access}</li>
-            <li>Any future community events</li>
-            <li>Open guest policy</li>
+            {#if tier === "Dedicated Desk"}
+                <li>A personal set of keys</li>
+            {/if}
+            <li>Invitations to private events</li>
+            <li>Priority access to our monthly Community Dinners</li>
+            <li>Discounted rates for evening/weekend space rentals</li>
+            {#if tier === "Dedicated Desk" || tier === "Hot Desk"}
+                <li>One complimentary event per month</li>
+            {/if}
         </ul>
     </div>
     <div class="row-span-1 mt-4 text-center">
-        <p class="font-bold text-lg lg:text-xl">${price}/month + HST</p>
-        <p class="text-m lg:text-lg">{cancellationPolicy}</p>
+        <p class="font-bold text-m lg:text-xl">${price}/month + HST</p>
+        <p class="text-sm lg:text-lg">{cancellationPolicy}</p>
     </div>
 </div>
